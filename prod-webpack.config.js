95fc7d3a73954a4c9ce156b281a705e2946e15cc
Richard Duivenvoorde <richard@duif.net>
2021-05-12 13:56:19 +0200

    init project

---
+++ prod-webpack.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+
+const themeEntries = require('./MapStore2/build/themes.js').themeEntries;
+const extractThemesPlugin = require('./MapStore2/build/themes.js').extractThemesPlugin;
+const ModuleFederationPlugin = require('./MapStore2/build/moduleFederation').plugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+    base: __dirname,
+    dist: path.join(__dirname, "dist"),
+    framework: path.join(__dirname, "MapStore2", "web", "client"),
+    code: [path.join(__dirname, "js"), path.join(__dirname, "MapStore2", "web", "client")]
+};
+
+module.exports = require('./MapStore2/build/buildConfig')(
+    {
+        'ngein': path.join(__dirname, "js", "app"),
+        'ngein-embedded': path.join(__dirname, "js", "embedded"),
+        'ngein-api': path.join(__dirname, "MapStore2", "web", "client", "product", "api"),
+        'geostory-embedded': path.join(__dirname, "js", "geostoryEmbedded"),
+        "dashboard-embedded": path.join(__dirname, "js", "dashboardEmbedded")
+    },
+    themeEntries,
+    paths,
+    [extractThemesPlugin, ModuleFederationPlugin],
+    true,
+    "dist/",
+    '.ngein',
+    [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'indexTemplate.html'),
+            chunks: ['ngein'],
+            inject: "body",
+            hash: true
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'embeddedTemplate.html'),
+            chunks: ['ngein-embedded'],
+            inject: "body",
+            hash: true,
+            filename: 'embedded.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'apiTemplate.html'),
+            chunks: ['ngein-api'],
+            inject: 'head',
+            hash: true,
+            filename: 'api.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'geostory-embedded-template.html'),
+            chunks: ['geostory-embedded'],
+            inject: "body",
+            hash: true,
+            filename: 'geostory-embedded.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'dashboard-embedded-template.html'),
+            chunks: ['dashboard-embedded'],
+            inject: 'body',
+            hash: true,
+            filename: 'dashboard-embedded.html'
+        })
+    ],
+    {
+        "@mapstore/patcher": path.resolve(__dirname, "node_modules", "@mapstore", "patcher"),
+        "@mapstore": path.resolve(__dirname, "MapStore2", "web", "client"),
+        "@js": path.resolve(__dirname, "js")
+    }
+);
